1ec722e34cd30db17301cf22777634ae4745b4a6
Jordan Porter <jporter@ngacres.com>
2018-01-10 16:49:35 -0700

    finished

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 export default class Home extends Component {
 
-  render() {
-    return (
-      <div className="box">
-        <button className='btn'>Math 1010</button>
-        <button className='btn'>English 2010</button>
-        <button className='btn'>Biology 2020</button>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="box">
+                <Link to="/classlist/MATH1010">
+                    <button className='btn'>Math 1010</button>
+                </Link>
+                <Link to="/classlist/ENG2010">
+                    <button className='btn'>English 2010</button>
+                </Link>
+                <Link to="/classlist/BIO2020">
+                    <button className='btn'>Biology 2020</button>
+                </Link>
+            </div>
+        );
+    }
 }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class Student extends Component {
     constructor() {
       super()
+        this.state = {
+          studentInfo: {}
+        }
 
+    }
+
+    componentDidMount(){
+        axios.get(`http://localhost:3005/students/${this.props.match.params.id}`)
+            .then((resp)=>{
+                this.setState({
+                    studentInfo: resp.data
+                })
+            })
     }
 
     render() {
       return (
         <div className="box">
           <h1>Student</h1>
+            <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+            <h3>Grade: {this.state.studentInfo.grade}</h3>
+            <h3>Email: {this.state.studentInfo.email}</h3>
         </div>
       )
     }
