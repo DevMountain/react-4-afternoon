e060d64849cefd5af9167e05dcd3fc5c94f7229f
Jordan Porter <jporter@ngacres.com>
2018-01-10 16:52:20 -0700

    finished

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Link, Switch, Route} from 'react-router-dom';
+import Contact from '../Contact/Contact';
+import History from '../History/History'
+
 
 export default class About extends Component {
 
@@ -17,7 +21,16 @@ export default class About extends Component {
                         <Route path='/about' render={() => (
                             <div>
                                 <h1>About the University</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eu lorem et ultricies. In porta lorem at dui semper porttitor. Nullam quis cursus dui. Cras tincidunt vehicula tellus eu facilisis. Donec nisi turpis, iaculis et arcu a, aliquet ultrices nisl. Nam in pharetra odio, ac blandit metus. Suspendisse potenti. Praesent elementum diam non orci cursus rutrum. Pellentesque condimentum ultrices dignissim. Sed a tempor ligula, vel luctus sapien. Mauris vehicula rutrum massa. Duis condimentum, ex quis ullamcorper rhoncus, erat libero tempor arcu, condimentum facilisis tellus lectus ut nunc. Pellentesque vitae faucibus diam. Vestibulum eu erat ex. Ut justo neque, varius aliquet erat vel, scelerisque convallis lacus. Mauris semper lorem mauris, sed dignissim eros consectetur nec.</p>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod eu lorem et
+                                    ultricies. In porta lorem at dui semper porttitor. Nullam quis cursus dui. Cras
+                                    tincidunt vehicula tellus eu facilisis. Donec nisi turpis, iaculis et arcu a,
+                                    aliquet ultrices nisl. Nam in pharetra odio, ac blandit metus. Suspendisse potenti.
+                                    Praesent elementum diam non orci cursus rutrum. Pellentesque condimentum ultrices
+                                    dignissim. Sed a tempor ligula, vel luctus sapien. Mauris vehicula rutrum massa.
+                                    Duis condimentum, ex quis ullamcorper rhoncus, erat libero tempor arcu, condimentum
+                                    facilisis tellus lectus ut nunc. Pellentesque vitae faucibus diam. Vestibulum eu
+                                    erat ex. Ut justo neque, varius aliquet erat vel, scelerisque convallis lacus.
+                                    Mauris semper lorem mauris, sed dignissim eros consectetur nec.</p>
                             </div>
                         )}/>
                     </Switch>
@@ -25,5 +38,5 @@ export default class About extends Component {
             </div>
         )
     }
-    
+
 }
