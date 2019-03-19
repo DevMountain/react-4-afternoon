f90ee3e1a284ac399220d54207e2cf51969c1bca
Beth Telford <mrsbethtelford@gmail.com>
2019-03-19 14:35:32 -0600

    move HashRouter

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import routes from './routes';
+import React, { Component } from "react";
+import { HashRouter, Link } from "react-router-dom";
+import routes from "./routes";
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <nav className='nav'>
-          <div>WestSide University</div> 
-          <div className='link-wrap'>
-            <Link to="/" className='links'>Home</Link>
-            <Link to="/about" className='links'>About</Link>
-          </div>
-        </nav>
-
-        { routes }
-      </div>
-    )
+      <HashRouter>
+        <div>
+          <nav className="nav">
+            <div>WestSide University</div>
+            <div className="link-wrap">
+              <Link to="/" className="links">
+                Home
+              </Link>
+              <Link to="/about" className="links">
+                About
+              </Link>
+            </div>
+          </nav>
+          {routes}
+        </div>
+      </HashRouter>
+    );
   }
 }
